6669b56ae3b3f0cc1d37fe7fcc9ad2fb9083191d
raf <rafhall@hotmail.com>
2025-06-03 10:47:09 +0100

    Extract basic information about single plant .

--- pipeline_live_data/extract.py
+++ pipeline_live_data/extract.py
@@ -1,4 +1,22 @@
 """Extracting plant data from the API."""
 
+import aiohttp
+import asyncio
+
+BASE_URL = 'https://sigma-labs-bot.herokuapp.com/api/plants/'
+
+
+async def extract_plant_data():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{BASE_URL}50') as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.json()
+            print(html)
+
+
 if __name__ == "__main__":
-    pass
+    asyncio.run(extract_plant_data())
