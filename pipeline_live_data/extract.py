a20c45757253ce78da817828f5e602d5b8c696f7
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-04 10:59:45 +0100

    Lint extract.py

--- pipeline_live_data/extract.py
+++ pipeline_live_data/extract.py
@@ -12,7 +12,7 @@ import aiohttp
 logger = logging.getLogger(__name__)
 
 logging.basicConfig(
-    level="DEBUG", 
+    level="DEBUG",
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     datefmt="%Y-%m-%dT%H:%M:%S"
 )
@@ -39,7 +39,10 @@ async def extract_plant_data(plant_id_start: int = 1,
         plant_data = await asyncio.gather(*plants)
 
     logger.info(
-        f"Completed API calls for plants with plant_id in the range {plant_id_start} - {plant_id_end}."
+        "Completed API calls for plants with plant_id in the range "
+        "{%s} - {%s}.",
+        plant_id_start,
+        plant_id_end
         )
 
     return plant_data
@@ -49,7 +52,8 @@ async def extract_single_plant_data(plant_id: int, session: aiohttp.ClientSessio
     """Extracts plant data for a given id. """
 
     logger.debug(
-        f"Started function extract_single_plant() for plant_id {plant_id}."
+        "Started function extract_single_plant() for plant_id %s.",
+        plant_id
         )
 
     response = await session.get(f'{BASE_URL}{plant_id}')
@@ -60,14 +64,18 @@ async def extract_single_plant_data(plant_id: int, session: aiohttp.ClientSessio
         html['received_at'] = datetime.now(
             timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
-    logger.info(f"Made API call for plant_id: {plant_id}. Status code: {response.status}.")
+    logger.info("Made API call for plant_id: %s. Status code: %s.",
+                plant_id,
+                response.status)
 
     if not html["plant_id"]:
         logger.warning(
-            f"Received unexpected result. Request to {BASE_URL}{plant_id} missing plant_id key.")
+            "Received unexpected result. Request to %s missing plant_id key.",
+            f"{BASE_URL}{plant_id}")
 
     logger.debug(
-        f"Finished function extract_single_plant() for plant_id {plant_id}."
+        "Finished function extract_single_plant() for plant_id %s.",
+        plant_id
     )
 
     return html
