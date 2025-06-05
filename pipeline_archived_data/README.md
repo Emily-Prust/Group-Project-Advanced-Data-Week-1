ac3e5fc06c1e6dc4250384ed71772a13a890b987
raf <rafhall@hotmail.com>
2025-06-05 12:04:44 +0100

    Successful connection to db in extract.

--- pipeline_archived_data/README.md
+++ pipeline_archived_data/README.md
@@ -1,2 +1,23 @@
 # `/pipeline_archived_data`
 
+## Setup
+
+Run the following command in a terminal to download the necessary driver:
+
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
+brew update
+HOMEBREW_ACCEPT_EULA=Y brew install msodbcsql18 mssql-tools18
+```
+
+Create a `.env` with the following keys:
+
+```
+DB_HOST=XXXXXXXXXX
+DB_PORT=1433
+DB_NAME=XXXXXXXXXX
+DB_USER=XXXXXXXXXX
+DB_PASSWORD=XXXXXXXXXX
+DB_DRIVER=ODBC Driver 18 for SQL Server
+```
--- pipeline_archived_data/extract.py
+++ pipeline_archived_data/extract.py
@@ -1,4 +1,38 @@
 """Extract script."""
+# pylint: disable=I1101
+
+from os import environ as ENV
+import pyodbc
+import pandas as pd
+
+from dotenv import load_dotenv
+
+
+def get_connection():
+    """Get a connection to the database."""
+
+    conn_str = (f"DRIVER={{{ENV['DB_DRIVER']}}};SERVER={ENV['DB_HOST']};"
+                f"PORT={ENV['DB_PORT']};DATABASE={ENV['DB_NAME']};"
+                f"UID={ENV['DB_USER']};PWD={ENV['DB_PASSWORD']};Encrypt=no;")
+
+    return pyodbc.connect(conn_str)
+
+
+def get_plant_data(conn: pyodbc.connect) -> pd.DataFrame:
+    """Returns all the data in the RDS older than 24 hours."""
+
+    # Testing we get a successful connection to the db.
+    with conn.cursor() as cur:
+        q = """SELECT table_name, table_schema FROM INFORMATION_SCHEMA.TABLES
+               WHERE TABLE_TYPE='BASE TABLE';"""
+        cur.execute(q)
+        data = cur.fetchone()
+
+    conn.close()
+    return list(data)
+
 
 if __name__ == "__main__":
-    pass
+
+    load_dotenv()
+    print(get_plant_data(get_connection()))
