bef8f621da05b6346af68d3ceaa5de8b6c5e7d22
raf <rafhall@hotmail.com>
2025-06-05 13:34:18 +0100

    Loading to bucket works with dummy file.

--- pipeline_archived_data/load.py
+++ pipeline_archived_data/load.py
@@ -1,4 +1,18 @@
 """Load script."""
 
+from os import environ as ENV
+import boto3
+from dotenv import load_dotenv
+
+from transform import get_file_data
+
 if __name__ == "__main__":
-    pass
+
+    load_dotenv()
+
+    info = get_file_data()
+
+    s3_client = boto3.client(
+        's3', aws_access_key_id=ENV["AWS_ACCESS_KEY"], aws_secret_access_key=ENV["AWS_SECRET_KEY"])
+    s3_client.upload_file(
+        info["file_name"], ENV["BUCKET_NAME"], info["bucket_key"])
--- pipeline_archived_data/transform.py
+++ pipeline_archived_data/transform.py
@@ -10,5 +10,27 @@ Assuming we have a dataframe... :
 """
 
 
+from datetime import datetime, timedelta, timezone
+import pandas as pd
+
+
+def get_file_data() -> dict:
+    """Returns a dict of file specific data."""
+
+    current_time = datetime.now(timezone.utc)
+    start_window = current_time - timedelta(hours=24)
+    year = start_window.strftime('%Y')
+    month = start_window.strftime('%m')
+    day = start_window.strftime('%d')
+
+    filename = f'plants_{start_window.strftime('%H_%d-%m-%Y')}.csv'
+
+    return {
+        "start_window": start_window,
+        "file_name": filename,
+        "bucket_key": f"{year}/{month}/{day}/{filename}"
+    }
+
+
 if __name__ == "__main__":
-    pass
+    print(get_file_data())
