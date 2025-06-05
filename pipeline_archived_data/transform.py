0cbc4285f837f37b2fa71b61a0d0f9ce616d1db9
raf <rafhall@hotmail.com>
2025-06-05 10:37:30 +0100

    Add requirements and plan.

--- pipeline_archived_data/transform.py
+++ pipeline_archived_data/transform.py
@@ -1,4 +1,14 @@
-"""Transform script."""
+"""Transform script.
+
+TEMPORARY:
+Extracting gives us:
+- a pandas dataframe containing plant data, extracted once an hour
+- bound by anything older than (current time - 24 / 23 hours) -> 24 hours at most in rds?
+    - calculated from the received_at and recording_taken columns
+
+Assuming we have a dataframe... :
+"""
+
 
 if __name__ == "__main__":
     pass
