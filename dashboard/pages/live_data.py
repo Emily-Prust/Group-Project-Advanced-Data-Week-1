5504ec0dd5ff86f34f3e0daacd70b9057c3ebb73
Em <trainee.emily.prust@sigmalabs.co.uk>
2025-06-06 15:40:35 +0100

    Create live dashboard page

--- dashboard/pages/live_data.py
+++ dashboard/pages/live_data.py
@@ -1,10 +1,10 @@
 """The dashboard page for real-time sensor data."""
 
-import numpy as np
 import pandas as pd
 import streamlit as st
 
-from resources.live_data.charts import line_graph_temperature, line_graph_soil_moisture
+from resources.live_data.charts import (line_graph_temperature, line_graph_soil_moisture,
+                                        pie_chart_oob_errors)
 from resources.live_data.extract import get_mock_data
 
 
@@ -30,54 +30,90 @@ def user_filtering(data_to_filter: pd.DataFrame):
     return data
 
 
-if __name__ == "__main__":
+def create_oob_information(measurement_data: pd.DataFrame) -> pd.DataFrame:
+    """Creates out of bounds information to plot in a pie chart."""
+    temp_lower_bound = 10
+    temp_upper_bound = 30
+    moisture_lower_bound = 20
+    moisture_upper_bound = 100
+
+    no_issue_count = measurement_data[
+        ((measurement_data["temperature"] > temp_lower_bound) &
+         (measurement_data["temperature"] < temp_upper_bound))
+        & ((measurement_data["soil_moisture"] > moisture_lower_bound) &
+           (measurement_data["soil_moisture"] < moisture_upper_bound))].shape[0]
+
+    temperature_issue_count = measurement_data[
+        ~ ((measurement_data["temperature"] > temp_lower_bound) &
+           (measurement_data["temperature"] < temp_upper_bound))
+        & ((measurement_data["soil_moisture"] > moisture_lower_bound) &
+           (measurement_data["soil_moisture"] < moisture_upper_bound))].shape[0]
+
+    soil_moisture_issue_count = measurement_data[
+        ((measurement_data["temperature"] > temp_lower_bound) &
+         (measurement_data["temperature"] < temp_upper_bound))
+        & ~ ((measurement_data["soil_moisture"] > moisture_lower_bound) &
+             (measurement_data["soil_moisture"] < moisture_upper_bound))].shape[0]
+
+    multiple_issues_count = measurement_data[
+        ~ ((measurement_data["temperature"] > temp_lower_bound) &
+           (measurement_data["temperature"] < temp_upper_bound))
+        & ~ ((measurement_data["soil_moisture"] > moisture_lower_bound) &
+             (measurement_data["soil_moisture"] < moisture_upper_bound))].shape[0]
+
+    return pd.DataFrame({"oob_status": ["No Issue",
+                                                   "Temperature Issue",
+                                                   "Soil-Moisture Issue",
+                                                   "Multiple Issues"],
+                                    "count": [no_issue_count,
+                                              temperature_issue_count,
+                                              soil_moisture_issue_count,
+                                              multiple_issues_count]})
+
+
+def main_display(measurement_data: pd.DataFrame):
+    """Elements displayed at the top of the page."""
+
+    st.dataframe(measurement_data[["plant_name",
+                                    "temperature",
+                                     "soil_moisture",
+                                    "last_watered"]],
+                  height=180)
+
+    right_subcolumn, left_subcolumn = st.columns([1, 1])
+
+    with right_subcolumn:
+        st.altair_chart(line_graph_temperature(measurement_data))
+
+    with left_subcolumn:
+        st.altair_chart(line_graph_soil_moisture(measurement_data))
+
+
+def main():
 
     st.set_page_config(layout="wide",
                        initial_sidebar_state="collapsed")
 
-    st.title("LMNH Botanical Live Dashboard")
-
     filename = "resources/live_data/test_plants_extra.csv"
     data = get_mock_data(filename)
 
+    st.title("LMNH Botanical Live Dashboard")
     left_column, right_column = st.columns([1, 3])
+
 
     with left_column:
         data = user_filtering(data)
         measurement_data = data[["plant_id", "plant_name", "temperature", "soil_moisture",
                                  "last_watered", "at", ]].dropna()
         error_data = data[["plant_id", "error_name"]]
-        # Pie chart for high-level errors.
-        # Pie chart for measurements.
+        
+        oob_information = create_oob_information(measurement_data)
+
+        st.altair_chart(pie_chart_oob_errors(oob_information))
 
     with right_column:
-
-        st.dataframe(measurement_data[["plant_name", 
-                                       "temperature", 
-                                       "soil_moisture",
-                                       "last_watered"]],
-                    height=180)
-        st.dataframe(error_data,
-                     height=180)
-        right_subcolumn, left_subcolumn = st.columns([1,1])
-        with right_subcolumn:
-            st.altair_chart(line_graph_temperature(measurement_data))
-        with left_subcolumn:
-            st.altair_chart(line_graph_soil_moisture(measurement_data))
+        main_display(measurement_data)
 
 
-    _comment = """
-    Necessary pieces:
-    [input] Botanist selection
-        In: list of botanist names.
-        Out: Botanist selection widget
-        [filter the data based on selected botanist]
-
-    [pie chart] Number of string errors
-    [pie chart] Number of OOB errors
-    [table] Live plant data
-    [table] Live error data
-    [line graph] Temperature over time
-    [line graph] Soil moisture over time
-
-    """
+if __name__ == "__main__":
+    main()
--- dashboard/pages/resources/live_data/charts.py
+++ dashboard/pages/resources/live_data/charts.py
@@ -4,18 +4,26 @@ import pandas as pd
 import altair as alt
 
 
-def pie_chart_sensor_errors() -> alt.Chart:
+def pie_chart_sensor_errors(sensor_error_data: pd.DataFrame) -> alt.Chart:
     """Charts the number of plants with errors or that are missing."""
-    pass
+
+    # return alt.Chart(payment_method_data).mark_arc().encode(
+    #     theta=alt.Theta("count", title="Transaction Count"),
+    #     color=alt.Color("payment_method", title="Payment Method")
+    
 
 
-def pie_chart_oob_errors() -> alt.Chart:
+def pie_chart_oob_errors(oob_reading_data: pd.DataFrame) -> alt.Chart:
     """Charts the number of plants with problematic measurements."""
-    pass
+
+    return alt.Chart(oob_reading_data).mark_arc().encode(
+        theta=alt.Theta("count", title="Issue Count"),
+        color=alt.Color("oob_status", title="Issue"))
 
 
 def line_graph_temperature(plant_data: pd.DataFrame) -> alt.Chart:
     """Charts the temperature over time for a number of plants."""
+
     return alt.Chart(plant_data).mark_line().encode(
         x=alt.X("at", title="Time", type="temporal"),
         y=alt.Y("temperature", title="Temperature"),
@@ -25,6 +33,7 @@ def line_graph_temperature(plant_data: pd.DataFrame) -> alt.Chart:
 
 def line_graph_soil_moisture(plant_data: pd.DataFrame) -> alt.Chart:
     """Charts the soil moisture over time for a number of plants."""
+
     return alt.Chart(plant_data).mark_line().encode(
         x=alt.X("at", title="Time", type="temporal"),
         y=alt.Y("soil_moisture", title="Soil Moisture"),
