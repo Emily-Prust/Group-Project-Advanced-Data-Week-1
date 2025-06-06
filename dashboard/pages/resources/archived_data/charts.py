868a9eecd6c5932eb9091a5f62a6f62d9cbeb6c6
tomkollig <195304969+tomkollig@users.noreply.github.com>
2025-06-06 11:38:16 +0100

    load script

--- dashboard/pages/resources/archived_data/charts.py
+++ dashboard/pages/resources/archived_data/charts.py
@@ -1,1 +1,6 @@
 """Functions for creating visualisations of the archived data."""
+
+import altair as alt
+import pandas as pd
+
+from extract import load_data
--- dashboard/pages/resources/archived_data/extract.py
+++ dashboard/pages/resources/archived_data/extract.py
@@ -1,1 +1,16 @@
 """Functions for extracting and caching the archived data."""
+import pandas as pd
+
+
+def load_data(file_name: str) -> pd.DataFrame:
+    """Loading the data to a dataframe.
+    Currently reads from a csv, can be changed to loading from RDS in future.
+    """
+    return pd.read_csv(file_name)
+
+
+if __name__ == "__main__":
+    df = load_data("test_plants_historical.csv")
+
+    print(df.head())
+    print(df.describe())
